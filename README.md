7240782c35d29b78897f69e809f836b27b3be634
Vasily Belosludcev <bupy765@gmail.com>
2017-11-29 22:56:32 +0500

    1.1.0

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Usage
 ```html
 <input id="money" type="text" value="542.23">
 
-<script type="text/javascript" src="/PATH_TO_BOWER/js-lib-url/dist/money-input.min.js"></script>
+<script type="text/javascript" src="js-lib-url/dist/money-input.min.js"></script>
 <script type="text/javascript">
 var moneyInput = new MoneyInput('#money');
 </script>
@@ -33,11 +33,12 @@ var moneyInput = new MoneyInput('#money');
 Options
 -------
 
-MoneyInput class have three parameters:
+MoneyInput class have four parameters:
 
 - `input` (string|object) - Input selector or HTML-object.
 - `round` (?integer) - Number of decimal digits to round to. *[optional]*
 - `negative` (?boolean) - Whether or not contain negative value. *[optional]*
+- `init` (?boolean) - Whether autoinit formatter. *[optional]*
 
 License
 -------
--- src/money-input.js
+++ src/money-input.js
@@ -1,34 +1,70 @@
 var MoneyInput = (function() {
     'use strict';
+    
     /**
      * @param {string|object} input Input selector or object.
      * @param {?integer} round Number of decimal digits to round to.
      * @param {?boolean} negative Whether or not contain negative value.
+     * @param {?boolean} init Whether autoinit formatter.
      * @returns {Money}
      */
-    function MoneyInput(input, round, negative) {
+    function MoneyInput(input, round, negative, init) {
         this.input = typeof input == 'string' ? document.querySelector(input) : input;
-        this.round = round || 4;
-        this.negative = negative || true;
-        this.init();
+        this.round = typeof round === 'undefined' ? 4 : round;
+        this.negative = typeof negative === 'undefined' ? true : negative;
+        
+        if (typeof init === 'undefined' ? true : init) {
+            this.init();
+        }
     }
+    
     MoneyInput.prototype.init = function() {
         this.format();
-        this._attachEvents();
+        this.attachEvents();
     };
-    MoneyInput.prototype.format = function() {
-        var value = this.input.value;
+    MoneyInput.prototype.format = function(event) {
+        var value = this.input.value, 
+        	negative = false;
+            
         if (this.negative) {
-            var negative = /^-/.test(value);
+            negative = /^-/.test(value);
         }
-        value = value.replace(/(,|\.|\s)/g, '.')
-            .replace(/[^(\d|\.)]/g, '')
-            .replace(/(\.[\d]*)(\.)$/, '$1')
-            .replace(new RegExp('(\\.[\\d]{' + this.round + '})([\\d]+)$'), '$1');
-        this.input.value = negative ? '-' + value : value;
+        
+        value = value.replace(/(,|\.|\s)/g, '.');
+        var parts = value.split('.', 2),
+        	result = [];
+      	
+        if (parts.length > 0) {
+            result.push(parts[0].replace(/[^\d]/g, ''));
+            
+            if (parts[1] !== undefined) {
+            	var scale = parts[1].match(new RegExp('^[\\d]{0,' + this.round + '}'));
+                if (scale !== null) {
+              	    result.push(scale[0]);
+                }
+            }
+        }
+
+        var prefix = negative ? '-' : '';
+        
+        this.input.value = prefix + result.join('.');
     };
-    MoneyInput.prototype._attachEvents = function() {
+    
+    /**
+     * @since 1.1.0
+     */
+    MoneyInput.prototype.attachEvents = function() {
         this.input.addEventListener('keyup', this.format.bind(this));
+        this.input.addEventListener('change', this.format.bind(this));
     };
+    
+    /**
+     * @since 1.1.0
+     */
+    MoneyInput.prototype.detachEvents = function () {
+        this.input.removeEventListener('keyup', this.format.bind(this));
+        this.input.removeEventListener('change', this.format.bind(this));
+    };
+
     return MoneyInput;
 })();
